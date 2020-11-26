a92eb3a3eee1b40823f6e14103432401545f3fce
efiVanso <efi.vanso@gmail.com>
2020-11-26 19:36:19 +0200

    access to textInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import { TextInput, StyleSheet } from 'react-native'
+import React, {useState} from 'react'
+import { TextInput, StyleSheet,View, Text } from 'react-native'
 
 export default function TextScreen() {
-    return (
-        <TextInput
-        autoCapitalize="none"// for case like user name {IOS}
-        autoCorrect={false}// "
-        style={styles.input}/>
-    )
+    const [password, setPassword] = useState('');
+
+  return (
+    <View>
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newValue => setPassword(newValue)}// necessary to access to text value
+      />
+      {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
+    </View>
+  );
 }
 const styles = StyleSheet.create({
     input:{
